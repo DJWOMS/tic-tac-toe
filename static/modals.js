5eee81e2c98689de95902bc08ae06c07b8dd8a1c
Omelchenko Michael <socanime@gmail.com>
2022-08-25 02:18:53 +0300

    style form login

--- static/modals.js
+++ static/modals.js
@@ -1,8 +1,14 @@
 const modalSignup = document.getElementById("modal-signup")
+const modalLogin = document.getElementById("modal-login")
 
 document.querySelector(".signup").addEventListener('click', modalSignupOpen)
 document.getElementsByClassName("close")[0].addEventListener('click', modalSignupClose)
 document.querySelector(".form-signup").addEventListener('submit', sendFormSignup)
+
+document.querySelector(".login").addEventListener('click', modalLoginOpen)
+document.getElementsByClassName("close")[0].addEventListener('click', modalLoginClose)
+document.querySelector(".form-login").addEventListener('submit', sendFormLogin)
+
 
 function modalSignupOpen() {
     modalSignup.style.display = "block"
@@ -12,9 +18,19 @@ function modalSignupClose() {
     modalSignup.style.display = "none"
 }
 
+function modalLoginOpen() {
+    modalLogin.style.display = "block"
+}
+
+function modalLoginClose() {
+    modalLogin.style.display = "none"
+}
+
 window.onclick = function (event) {
     if (event.target === modalSignup) {
         modalSignup.style.display = "none"
+    } else if (event.target === modalLogin) {
+        modalLogin.style.display = "none"
     }
 }
 
@@ -32,7 +48,28 @@ function sendFormSignup(event) {
         mode: 'cors',
         headers: {
             'Content-Type': 'application/json'
-            // 'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(error => alert(error))
+}
+
+function sendFormLogin(event) {
+    event.preventDefault()
+    let data = {}
+    let formData = new FormData(this)
+    formData.forEach(function (value, key) {
+        data[key] = value;
+    });
+
+    console.log('this', data)
+    fetch('http://127.0.0.1:8000/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Content-Type': 'application/json'
         },
         body: JSON.stringify(data)
     })
