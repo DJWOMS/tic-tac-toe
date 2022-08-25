ad1fd6572960f6388b7178cf5e715394287fc947
Omelchenko Michael <socanime@gmail.com>
2022-08-25 16:12:46 +0300

    update user services

--- src/auth/models.py
+++ src/auth/models.py
@@ -7,9 +7,11 @@ class User(Base):
     __tablename__ = 'users'
 
     id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
+    name = Column(String, nullable=False, unique=True)
+    email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
     win = Column(Integer, default=0)
     loss = Column(Integer, default=0)
     draw = Column(Integer, default=0)
+
+    __mapper_args__ = {"eager_defaults": True}
---
+++ src/auth/service.py
@@ -0,0 +1,69 @@
+from typing import List, Optional
+
+from sqlalchemy.orm import Session
+from sqlalchemy import update, or_
+from sqlalchemy.future import select
+
+from config.db import async_session
+from .validators import get_password_hash
+from .models import User
+
+
+async def get_session():
+    async with async_session() as session:
+        async with session.begin():
+            yield UserService(session)
+
+
+class UserService:
+    def __init__(self, db_session: Session):
+        self.db_session = db_session
+
+    @classmethod
+    async def ainit(cls):
+        async with async_session() as session:
+            async with session.begin():
+                return cls(session)
+
+    async def create_user(self, **kwargs):
+        new_user = User(
+            name=kwargs.get('username'),
+            password=get_password_hash(kwargs.get('password1')),
+            email=kwargs.get('email')
+        )
+        self.db_session.add(new_user)
+        await self.db_session.commit()
+        self.db_session.close()
+        return new_user
+
+    async def get_all_users(self) -> List[User]:
+        query = await self.db_session.execute(select(User).order_by(User.id))
+        return query.scalars().all()
+
+    async def get_top_users(self) -> List[User]:
+        query = await self.db_session.execute(select(User).order_by(User.win))
+        return query.scalars().limit(10).all()
+
+    async def get_user(self, username: str) -> User:
+        query = await self.db_session.execute(select(User).where(User.name == username))
+        return query.scalars().first()
+
+    async def user_exist(self, name: str, email: str) -> bool:
+        query = await self.db_session.execute(select(User).filter(
+            or_(User.name == name, User.email == email)
+        ))
+        return query.scalars().first()
+
+    async def update_user(
+            self, user_id: int, win: Optional[str], loss: Optional[str], draw: Optional[int]
+    ) -> None:
+        query = update(User).where(User.id == user_id)
+        if win:
+            query = query.values(win=win)
+        if loss:
+            query = query.values(loss=loss)
+        if draw:
+            query = query.values(draw=draw)
+        query.execution_options(synchronize_session="fetch")
+        await self.db_session.execute(query)
+        self.db_session.close()
